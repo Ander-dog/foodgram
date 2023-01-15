63c7756900511fb0c51aee77b67c9ba6f07d378b
Андрей Алексеев <0306andrey@gmail.com>
2023-01-15 13:08:15 +0300

    Домашняя работа v2

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -118,8 +118,8 @@ DEFAULT_RECIPE_LIMIT = 5
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
